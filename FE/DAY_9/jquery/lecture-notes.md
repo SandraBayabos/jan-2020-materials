3c4a2f3bbd00d1370b03af80f15a0303166799be
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-22 15:22:42 +0800

    files for message board and jquery

---
+++ FE/DAY_9/jquery/lecture-notes.md
@@ -0,0 +1,7 @@
+### INTRODUCTION TO JQUERY
+
+1. What is Jquery?
+
+- It is a Javascript library that helps you to write shorter code. It's especially useful for selecting, styling and running animation-type functions
+- You need to import Jquery into your file before you can start using it!
+- No real best-practice about combining jquery and javascript together so do so if you need to or find certain things easier to do in vanilla JS as compared to Jquery
---
+++ FE/DAY_9/jquery/scripts.js
@@ -0,0 +1,36 @@
+$("#header").click(function() {
+  $("#header").css({ "background-color": "red" });
+});
+
+//vs
+
+const header = document.getElementById("header");
+
+header.addEventListener("click", () => {
+  header.style.backgroundColor = "red";
+});
+
+// Animation
+
+// clicks and fadeout
+$("p").click(function() {
+  $(this).fadeOut(1000);
+});
+
+// other examples
+
+// $("#second-para").mouseenter(function() {
+//   $(this)
+//     .css({ "font-size": "1.5rem" })
+//     .mouseleave(function() {
+//       $(this).css({ "font-size": "1rem" });
+//     });
+// });
+
+const secondPara = document.getElementById("second-para");
+secondPara.addEventListener("mouseover", () => {
+  secondPara.style.fontSize = "1.5rem";
+});
+secondPara.addEventListener("mouseleave", () => {
+  secondPara.style.fontSize = "1rem";
+});
