e316b07a9196ee0841757897180cda253f8899c9
Sandra Bayabos <sandra.bayabos@gmail.com>
2020-01-29 00:12:37 +0800

    lecture notes & demo

--- REACT/DAY_1/lecture_notes.md
+++ REACT/DAY_1/lecture_notes.md
@@ -12,3 +12,27 @@
 
 - String concatenation
   => using backticks is also an ES6 thing, which allows us to interpolate variables into our strings
+
+- Object Literals
+  => Basically shorthanding your objects whether in functions based on the param or because you've already defined the values of your keys before
+
+- Arrow Functions
+  => You've seen me use these a bunch of times before but we'll go through them again
+  => removes the need for the return keyword
+  => but if you're going to be using curly braces then you need to explicitly return
+
+- Looping/Array methods
+  => forEach: you've all done forEach loops because I've been using them in all my examples over the past two weeks, but the forEach loop is actually a new introduction from ES6
+  => But now we introduct you to a new type of loop or method that is specific to arrays called .map() and this will be very important for when you start actually working with react
+  => What .map() does is it makes a copy of an original array. So in react, when it comes to arrays, we can never reassign an array or mutate it as we call it, so no array.push(), array.pop(). The original array will always be as it is. So whenever we want to make a change to an original array, we first have to make a copy of it and only then do we modify it however we want.
+  => whenever we use .map() we ALWAYS have to return something otherwise our new array is going to be undefined
+  => filter() is a new type of method as well that allows us to filter through an array and return to us specific items that we want without having to use the index like in a traditional array
+
+3. What is destructuring?
+
+- destructuring is a way to shorten your code and also make things neater and more readable, especially when it comes to objects and arrays, and especially when we're constantly calling upon/using values from objects and arrays
+- we can prevent our code from looking too bloated
+- oftentimes we might write our code the normal, lengthy way, and then only destructure it afterwards, so I'd recommend that you guys do that so you become first comfortable with writing the code the way it's mean to be written, and then destructure it aftewards when you become more confident in it
+- this will also come in handy when you start passing props (properties/variables) between your different react components
+
+4. What is spread syntax?
--- REACT/DAY_1/scripts.js
+++ REACT/DAY_1/scripts.js
@@ -16,6 +16,69 @@ console.log(`My favourite fruits are ${fruit}`);
 
 console.log("My favourite fruits are " + fruit);
 
+// OBJECT LITERALS
+
+// original JS
+// create a function that returns an object
+
+function favouriteCharacter(name, age) {
+  return {
+    name: name,
+    age: age
+  };
+}
+
+const book = favouriteCharacter("Harry Potter", 12);
+console.log(book);
+
+// ES6
+// If what you are returning is the same as in the parameters then you don't need to repeat yourself
+// Just using shorthand
+const favouriteCharacter = (myName, myAge) => {
+  return { myName, myAge };
+};
+console.log(favouriteCharacter("Harry Potter", 25));
+
+// OR
+
+function extraCharacters(myName, myAge) {
+  return {
+    myName,
+    myAge
+  };
+}
+
+console.log(extraCharacters("Ron Weasley", 35));
+
+// Just with an object
+
+const personA = "Harry Potter";
+const personB = "Ron Weasley";
+
+const cast = {
+  personA,
+  personB
+};
+
+console.log(cast);
+
+const user = {
+  firstName: "Joey",
+  lastName: "Tribbiani",
+  printFullName: function fullName() {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+  actions: {
+    laugh: function() {
+      console.log("Muahaha");
+    },
+    getJoke: function() {
+      console.log("A lawyer walks into a bar exam");
+    },
+    hobbies: [["I like to eat"], [" love to sleep"], ["I am not real"]]
+  }
+};
+
 // FUNCTIONS
 
 // old JS
@@ -23,11 +86,121 @@ console.log("My favourite fruits are " + fruit);
 let name = "Adam";
 
 function sayHello() {
-  name = "Bella";
+  let name = "Bella";
   console.log(name);
 }
 // scope of name outside is global and scope of name inside is local so local scope can't be accessed outside
 sayHello(); // will return "Bella"
 console.log(name); // will return "Adam"
+// declaring variables is different from reassigning variables, so if I were to just say name = Bella then the function reassigns the original from "Adam" to "Bella"
 
 // ES6
+// here, we are essentially saving a function in a variable but we would call the function the same way
+let name = "Hermione";
+
+const sayHello = () => {
+  let name = "Luna";
+  return name;
+};
+
+console.log(sayHello()); // will return "Luna"
+console.log(name); // will return "Hermione" due to scoping unless I actually reassign name = "Luna"
+
+// arrow function with 1 parameter
+
+function sayMyName(name) {
+  return name;
+}
+
+console.log(sayMyName("Sandra")); // will return sandra
+
+// implicit return of name but ONLY if you're returning one thing i.e. in this case, we're only returning name
+const sayMyName = name => name;
+console.log(sayMyName("Jupiter"));
+
+// it's the same as:-
+
+const sayMyName = name => {
+  return name;
+};
+
+console.log(sayMyName("GG"));
+
+// arrow functions with more than one parameter
+// since we have more than one, we need to use curly braces and return the two params
+// need to return them in curly braces
+const userLocation = (city, country) => {
+  return `I live in the city of ${city} in ${country}`;
+};
+
+console.log(userLocation("Kuala Lumpur", "Malaysia"));
+
+// LOOPS
+
+// forEach with just one parameter, which is just each product within your array
+
+const shoppingList = ["Tomatoes", "Mushrooms", "Broccoli", "Kimchi", "Rice"];
+
+// if you only have one parameter, you don't need the parentheses around it
+
+shoppingList.forEach(product => {
+  console.log(`I have ${product} in my shopping bag`);
+});
+
+// forEach with two parameters
+// when you have two or more parameters, you need to add in the parentheses. such is the case for arrow functions
+
+shoppingList.forEach((product, index) => {
+  console.log(`The index for ${product} is ${index}`);
+});
+
+// Map
+const newList = shoppingList.map(item => {
+  console.log(item);
+});
+// if we treat it like a forEach, it's going to loop through it, but look what happens when we console.log newList => the array is going to be undefined!
+
+// SO, we need to return WITHIN the .map()
+const newerList = shoppingList.map(item => {
+  return item;
+});
+
+console.log(newerList); // will return to me the new array and now I can actually do stuff with THIS array, like add things to it, remove things, etc.
+
+// another way to write the above without the return would be like this:-
+// so we've implicitly returned item
+const newerList = shoppingList.map(item => item);
+console.log(newerList); // will still return to us the new array
+
+//filter
+const filterList = shoppingList.filter(item => {
+  return item === "Tomatoes"; // will return a new array with JUST tomatoes
+  // return item !== "Tomatoes"; // will return everything else other than Tomatoes
+});
+
+// DESTRUCTURING
+
+// destructuring an object
+
+const newUser = {
+  firstName: "Jupiter",
+  lastName: "Moon",
+  age: undefined
+};
+
+// so to get the elements inside i need to use the dot notation because it's an object and i can save them to variables
+
+const userFirstName = newUser.firstName; // will give me Jupiter
+const userLastName = newUser.lastName; // will give me Moon
+
+// but we can destructure it like this using the curly braces!
+
+const { firstName, lastName, age } = newUser;
+
+console.log(firstName, lastName, age);
+
+// i can also assign a new value to something when i destructure but ONLY if the original value was undefined
+
+const { firstName, lastName, age = 4 } = newUser;
+
+// SPREAD SYNTAX
